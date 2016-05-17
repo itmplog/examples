ec416ff074503dac530dcf083eb34b92461f3b3b
huaixiaoz <hello@itmp.top>
2016-05-17 21:21:43 +0800

    add FirstActivity

---
+++ app/src/main/java/top/itmp/examples/train/FirstActivity.java
@@ -0,0 +1,16 @@
+package top.itmp.examples.train;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import top.itmp.examples.R;
+import top.itmp.examples.base.BaseActivity;
+
+public class FirstActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+    }
+}
