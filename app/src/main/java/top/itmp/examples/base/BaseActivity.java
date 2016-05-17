69aa1afc7214ad6535fd78ca23d662a3aeb18b3b
huaixiaoz <hello@itmp.top>
2016-05-17 21:25:11 +0800

    change anims

--- app/src/main/java/top/itmp/examples/base/BaseActivity.java
+++ app/src/main/java/top/itmp/examples/base/BaseActivity.java
@@ -12,12 +12,14 @@ public class BaseActivity extends AppCompatActivity {
     @Override
     public void finish() {
         super.finish();
-        overridePendingTransition(R.anim.slide_in_top, R.anim.slide_out_bottom);
+        //overridePendingTransition(R.anim.slide_in_top, R.anim.slide_out_bottom);
+        overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
     }
 
     @Override
     public void startActivity(Intent intent) {
         super.startActivity(intent);
-        overridePendingTransition(R.anim.slide_in_bottom, R.anim.slide_out_top);
+        //overridePendingTransition(R.anim.slide_in_bottom, R.anim.slide_out_top);
+        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
     }
 }
