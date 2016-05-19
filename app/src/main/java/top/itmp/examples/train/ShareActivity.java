a85c0a2a66ab4be7dc041d800c130d01c1c636f9
huaixiaoz <hello@itmp.top>
2016-05-20 00:47:23 +0800

    ShareActivity

---
+++ app/src/main/java/top/itmp/examples/train/ShareActivity.java
@@ -0,0 +1,16 @@
+package top.itmp.examples.train;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import top.itmp.examples.base.BaseActivity;
+
+/**
+ * Created by hz on 16/5/20.
+ */
+public class ShareActivity extends BaseActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
