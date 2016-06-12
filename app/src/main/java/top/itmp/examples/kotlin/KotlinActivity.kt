92d2583000fdbca039b6dd608bcfcae020a74443
huaixiaoz <hello@itmp.top>
2016-06-13 00:54:01 +0800

    kotlin sample added

---
+++ app/src/main/java/top/itmp/examples/kotlin/KotlinActivity.kt
@@ -0,0 +1,13 @@
+package top.itmp.examples.kotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import top.itmp.examples.R
+
+class KotlinActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kotlin)
+    }
+}
