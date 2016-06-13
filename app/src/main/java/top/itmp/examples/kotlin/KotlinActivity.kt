52064dbd9f2ccdbb17a620d27421b03421a44593
huaixiaoz <hello@itmp.top>
2016-06-13 20:15:29 +0800

    anko sample

--- app/src/main/java/top/itmp/examples/kotlin/KotlinActivity.kt
+++ app/src/main/java/top/itmp/examples/kotlin/KotlinActivity.kt
@@ -2,12 +2,28 @@ package top.itmp.examples.kotlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import org.jetbrains.anko.*
 import top.itmp.examples.R
 
 class KotlinActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_kotlin)
+        //setContentView(R.layout.activity_kotlin)
+
+        verticalLayout {
+            padding = dip(30)
+            editText {
+                hint = "Name"
+                textSize = 24f
+            }
+            editText {
+                hint = "Password"
+                textSize = 24f
+            }
+            button("Login") {
+                textSize = 26f
+            }
+        }
     }
 }
